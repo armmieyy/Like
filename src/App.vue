58d5b70876f4ccb09bd74feb973e8bdd5b2c2cde
armmieyy <68525381+armmieyy@users.noreply.github.com>
2020-08-19 21:23:21 +0700

    init now

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to My World" />
+    <div class="row">
+      <div class="col-6">
+        <img alt="Vue logo" src="./assets/44544.png" />
+        <h1>{{total1}}</h1>
+      </div>
+      <div class="col-6">
+        <img alt="Vue logo" src="./assets/331120.jpg" />
+        <h1>{{total2}}</h1>
+      </div>
+      <div class="col-6">
+        <Counter @cc="sum" />
+      </div>
+      <div class="col-6">
+        <Counter @cc="sum1" />
+      </div>
+    </div>
+    <hr />
+    <!----ชื่อ สกึล รหัส GPA--->
+    ชื่อ-นามสกุล
+    <input type="text" v-model="name" />
+    รหัสนักศึกษา
+    <input type="text" v-model="code" />
+    GPA
+    <input type="number" v-model="gpa" step="0.01" />
+    <hr />
+    {{name}} {{code}} {{gpa}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Counter from "./components/Counter.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Counter,
+  },
+
+  data: function () {
+    return {
+      total1: 0,
+      total2: 0,
+      name: "",
+      code: "",
+      gpa: 0.1,
+    };
+  },
+  methods: {
+    sum(counterValue) {
+      this.total1 = counterValue;
+    },
+    sum1(counterValue) {
+      this.total2 = counterValue;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button v-on:click="counter += 1">{{label}}</button>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    label: String,
+  },
+  data: function () {
+    return {
+      counter: 0,
+      classObject: {
+        active: true,
+        "text-danger": false,
+        weight: 20,
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
